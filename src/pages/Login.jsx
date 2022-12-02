84a491187b7f10438864c1dd5e4b1e2d9b06c9a6
mbolonzi-design <mbolonzi.design@gmail.com>
2022-12-02 12:10:30 +0300

    create login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <form className='login-form'>
+        <label htmlFor="username">Username</label>
+        <input type="text" name='username' required/>
+        <button className="login-btn">Login</button>
+      </form>
+    </div>
   )
 }
 
