883b7e76be96ab352ab437048ff8a951b790ce19
mbolonzi-design <mbolonzi.design@gmail.com>
2022-12-15 21:23:56 +0300

    add  navbar links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav>
+      <div>
+        <ul>
+          <NavLink to='/'><li>Home</li></NavLink>
+          <NavLink to='/productlist'>Products</NavLink>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
